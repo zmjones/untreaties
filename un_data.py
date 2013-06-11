c56c1dcf3e83d3cceae5c2ee2546d483a523be11
Zachary M. Jones <zmj@zmjones.com>
2013-06-11 19:53:09 -0400

    parse XML instead of HTML, better error handling, removed HTML generator

--- un_data.py
+++ un_data.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 def read_page(url):
     mech = Browser()
-    page = mech.open(url)
+    page = mech.open(url, timeout = 60)
     html = page.read()
     soup = BeautifulSoup(html.decode("utf-8", "ignore"))
     return soup
@@ -75,33 +75,55 @@ def get_treaties(table_tag, base_url, treaty_list):
         sys.stdout.write(str(treaty) + " of " + str(len(treaty_list)) + " complete\r")
         sys.stdout.flush()
 
-def htmlize_treaty_list(treaty_list):
-    f = open('index.html', 'w')
-    data_url = "https://raw.github.com/zmjones/untreaties/master/data/"
-    f.write("""<html lang="en"><head><title></title></head><body><h1><center>U.N. Treaty Data</center></h1>""")
-    f.write("""<link rel="stylesheet" href="style.css" type="text/css"/>""")
-    f.write("""<p>This page contains all United Nations treaty data listed at 
-               <a href="http://treaties.un.org/pages/ParticipationStatus.aspx">treaties.un.org</a>.
-                The chapter and treaty links return you to the UN treaty or chapter page and the "CSV"
-                link downloads the treaty data. Utilities to clean and transform the data further, as 
-                well as the scripts that scraped this data can be found at
-                <a href="http://github.com/zmjones/untreaties">http://github.com/zmjones/untreaties</a></p>""")
-    f.write("""<table border="1" class="padded-table">""")
-    for i in range(0, len(treaty_list)):
-        if i == 0:
-            f.write("""<tr><th>chapter no.</th><th>chapter name</th><th>treaty no.</th>
-                       <th>treaty name</th><th>data</th></tr>""")
-        chap =  str(treaty_list[i][0])
-        treaty = str(treaty_list[i][1])
-        url = data_url + chap + "-" + treaty + ".csv"
-        f.write("<tr>")
-        f.write("<td>" + treaty_list[i][0] + "</td>")
-        f.write("<td><a href = \"" + treaty_list[i][4] + "\">" + treaty_list[i][5] + "</a></td>")
-        f.write("<td>" + treaty_list[i][1] + "</td>")
-        f.write("<td><a href = \"" + treaty_list[i][3] + "\">" + treaty_list[i][2] + "</a></td>")
-        f.write("<td><a href = \"" + url + "\">csv</a></td>")
-        f.write("</tr>")
-    f.write("</table></body></html>")
+def get_treaties(table_tag, base_url, treaty_list):
+    for treaty in range(0, len(treaty_list)):
+        soup = read_page(str(treaty_list[treaty][3]))
+        xml_link = soup.find(lambda tag:tag.name == "a" and
+                             tag.has_attr("id") and
+                             tag["id"] == "ctl00_ContentPlaceHolder1_lnkXml")
+        xml_link = xml_link["href"]
+        soup = read_page("http://treaties.un.org" + str(xml_link))
+        if soup is None:
+            print("URL error")
+            continue
+        table = soup.find(lambda tag:tag.name == "participants")
+        if table is None:
+            table = soup.find(lambda tag:tag.name == "specialtables")
+            if table is None or len(table) <= 1:
+                continue
+            df = []
+            for row in table.findAll("tableheader"):
+                labels = row.findAll("title")
+                for i in range(0, len(labels)):
+                    labels[i] = labels[i].get_text(strip = True)
+                    labels[i] = re.sub("\t|<title>|</title>", "", labels[i])
+                    labels[i] = unicodedata.normalize("NFKD", labels[i]).encode("ascii", "ignore")
+                df.append(labels)
+            for row in table.findAll("row"):
+                data = row.findAll("column")
+                for i in range(0, len(data)):
+                    data[i] = data[i].get_text(strip = True)
+                    data[i] = re.sub("\t|<superscript>|</superscript>", "", data[i])
+                    data[i] = unicodedata.normalize("NFKD", data[i]).encode("ascii", "ignore")
+                df.append(data)
+        else:
+            df = []
+            for row in table.findAll("row"):
+                data = row.findAll("entry")
+                for i in range(0, len(data)):
+                    data[i] = data[i].get_text(strip = True)
+                    if i == 0:
+                        data[i] = re.sub("\d|,", "", data[i])
+                    data[i] = re.sub("\t|<superscript>|</superscript>", "", data[i])
+                    data[i] = unicodedata.normalize("NFKD", data[i]).encode("ascii", "ignore")
+                df.append(data)
+
+        filename = str(treaty_list[treaty][0]) + "-" + str(treaty_list[treaty][1])
+        if not os.path.exists("data"):
+            os.makedirs("data")
+        pd.DataFrame(df).to_csv("data/" + filename + ".csv", header = False, index = False)
+        sys.stdout.write(str(treaty) + " of " + str(len(treaty_list)) + " complete\r")
+        sys.stdout.flush()
 
 base_url = "http://treaties.un.org/pages/"
 
@@ -113,5 +135,3 @@ treaty_list = get_treaty_list(chap_table_tag, base_url, chap_list)
             
 treaties_table_tag = "ctl00_ContentPlaceHolder1_tblgrid"
 get_treaties(treaties_table_tag, base_url, treaty_list)
-
-htmlize_treaty_list(treaty_list)
