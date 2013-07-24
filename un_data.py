fee34057dd93653ce0ccbabc93f841cf9d3d2060
Zachary M. Jones <zmj@zmjones.com>
2013-07-24 19:46:21 -0400

    refactor

--- un_data.py
+++ un_data.py
@@ -21,24 +21,25 @@ def read_page(url):
     return BeautifulSoup(html)
 
 
+def read_table(url, table_tag):
+    """
+    then parses the specified html table
+    """
+    soup = read_page(url)
+    return soup.find(lambda tag:tag.name == "table" and 
+                            tag.has_attr("id") and 
+                            tag["id"] == table_tag)
+
 def get_chap_list(table_tag, base_url):
     """
     parses the list of all UN treaty chapters
     """
-    soup = read_page(base_url + "ParticipationStatus.aspx")
-    table = soup.find(lambda tag:tag.name == "table" and 
-                                tag.has_attr("id") and 
-                                tag["id"] == table_tag)
-    df = []
-    for row in table.find_all("tr")[0:]:
-        col = row.find_all("td")
-        chapter = col[0].get_text(strip = True)
-        link = base_url + col[1].find("a").get("href")
-        name = col[2].get_text(strip = True).lower()
-        record = [chapter, link, name]
-        df.append(record)
-    print("chapter list successfully fetched")
-    return df
+    table = read_table(base_url + "ParticipationStatus.aspx", table_tag)
+    return [[td.get_text(strip=True)
+             if td.find("a") is None 
+             else base_url + td.find("a")["href"] 
+             for td in tr.find_all("td")] 
+             for tr in table.find_all("tr")]
 
 
 def get_treaty_list(table_tag, base_url, chap_list):
@@ -47,23 +48,28 @@ def get_treaty_list(table_tag, base_url, chap_list):
     """
     df = []
     for chap in range(len(chap_list)):
-        soup = read_page(str(chap_list[chap][1]))
-        table = soup.find(lambda tag:tag.name == "table" and
-                          tag.has_attr("id") and 
-                          tag["id"] == table_tag)
+        table = read_table(chap_list[chap][1], table_tag)
         for row in table.find_all("tr")[0:]:
             col = row.find_all("td")
-            number = col[0].get_text(strip = True)
-            number = re.sub(".$", "", str(number))
-            name = clean_entry(re.sub(r"\xc2|\xa0", "", col[1].get_text(strip = True))).lower()
+            number = re.sub("\\.$", "", col[0].get_text(strip=True))
+            name = clean(re.sub(r"\xc2|\xa0", "", col[1].get_text(strip=True)))
             url = base_url + col[1].find("a").get("href")
-            record = [chap_list[chap][0], number, name, url, chap_list[chap][1], chap_list[chap][2]]
-            df.append(record)
-    index_file = pd.DataFrame(df, columns = ["chapter_no", "treaty_no", 
-                                             "treaty_name", "url", "chapter_url", "chapter_name"])
-    index_file.to_csv("index.csv", index = False, encoding = "utf-8")
-    print("treaty list successfully fetched")
+            df.append([chap_list[chap][0], number, name, url, 
+                       chap_list[chap][1], chap_list[chap][2]])
+    cols = ["chapter_no", "treaty_no", "treaty_name", "url", "chapter_url", "chapter_name"]
+    pd.DataFrame(df, columns = cols).to_csv("index.csv", index=False, encoding="utf-8")
     return df
+
+
+def clean(s, head=False):
+    """
+    removes cruft from data entry
+    """
+    s = re.compile(r"<[^<]*?/?>|\t|\r|\n|\f|\[|\]").sub("", s)
+    s = re.sub(r"\s+|\xc2|\xa0", " ", s)
+    if head:
+        s = re.sub("\d|,", "", s)
+    return s.strip()
 
 
 def get_xml(treaty_url):
@@ -74,19 +80,7 @@ def get_xml(treaty_url):
     xml_link = soup.find(lambda tag:tag.name == "a" and
                          tag.has_attr("id") and
                          tag["id"] == "ctl00_ContentPlaceHolder1_lnkXml")
-    xml_link = xml_link["href"]
-    return read_page("http://treaties.un.org" + str(xml_link))
-
-
-def clean_entry(datum, head=False):
-    """
-    removes cruft from data entry
-    """
-    datum = re.compile(r"<[^<]*?/?>|\t|\r|\n|\f|\[|\]").sub("", datum)
-    datum = re.sub(r"\s+|\xc2|\xa0", " ", datum)
-    if head:
-        datum = re.sub("\d|,", "", datum)
-    return datum.strip()
+    return read_page("http://treaties.un.org" + str(xml_link["href"]))
 
 
 def get_normal_table(soup):
@@ -96,15 +90,11 @@ def get_normal_table(soup):
     table = soup.find(lambda tag:tag.name == "participants")
     if table is None:
         return None
-    df = []
-    for row in table.find_all("row"):
-        data = row.find_all("entry")
-        for i in range(len(data)):
-            data[i] = clean_entry(data[i].get_text(strip = True))
-            if i == 0:
-                data[i] = clean_entry(data[i], True)
-        df.append(data)
-    return df
+    return [[clean(td.get_text(strip=True))
+             if td.find("a") is None 
+             else base_url + td.find("a")["href"] 
+             for td in tr.find_all("entry")] 
+             for tr in table.find_all("row")]
 
 
 def get_special_table(soup):
@@ -114,21 +104,15 @@ def get_special_table(soup):
     table = soup.find(lambda tag:tag.name == "specialtables")
     if table is None:
         return None
-    df = []
-    for row in table.find_all("tableheader"):
-        labels = row.find_all("title")
-        for i in range(len(labels)):
-            labels[i] = labels[i].get_text(strip = True)
-            if i == 0:
-                labels[i] = clean_entry(labels[i], True)
-            labels[i] = clean_entry(labels[i])
-        df.append(labels)
-    for row in table.find_all("row"):
-        data = row.find_all("column")
-        for i in range(len(data)):
-            data[i] = clean_entry(data[i].get_text(strip = True))
-        df.append(data)
-    return df
+    header = [[clean(td.get_text(strip=True), True)
+               for td in tr.find_all("title")] 
+               for tr in table.find_all("tableheader")]
+    data = [[clean(td.get_text(strip=True))
+             if td.find("a") is None 
+             else base_url + td.find("a")["href"] 
+             for td in tr.find_all("column")] 
+             for tr in table.find_all("row")]
+    return header + data
 
 
 def get_declarations(soup, treaty_id):
@@ -138,13 +122,14 @@ def get_declarations(soup, treaty_id):
     table = soup.find(lambda tag:tag.name == "declarations")
     if table is not None:
         for row in table.find_all("declaration"):
-            name = clean_entry(row.find("participant").get_text(strip = True).lower(), True)
+            name = clean(row.find("participant").get_text(strip=True), True)
             text = row.find_all(lambda tag:tag.name == "text" and 
-                               tag.has_attr("type") and tag["type"] == "para")
+                                       tag.has_attr("type") and 
+                                       tag["type"] == "para")
             with open("declarations/" + treaty_id + ".txt", "a+") as f:
                 f.writelines("#" + name.encode("utf-8") + "\n")
                 for entry in text:
-                    entry = clean_entry(entry.get_text(strip = True))
+                    entry = clean(entry.get_text(strip=True))
                     f.writelines(entry.encode("utf-8"))
                 f.writelines("\n\n")
             f.close()
@@ -175,11 +160,14 @@ def get_treaties(base_url, treaty_list):
         sys.stdout.write(str(treaty) + " of " + str(len(treaty_list)) + " complete\r")
         sys.stdout.flush()
 
+
 base_url = "http://treaties.un.org/pages/"
 chap_list_table_tag = "ctl00_ContentPlaceHolder1_dgChapterList"
 chap_table_tag = "ctl00_ContentPlaceHolder1_dgSubChapterList"
 
 if __name__ == "__main__":
     chap_list = get_chap_list(chap_list_table_tag, base_url)
+    print("chapter list successfully fetched")
     treaty_list = get_treaty_list(chap_table_tag, base_url, chap_list)
+    print("treaty list successfully fetched")
     get_treaties(base_url, treaty_list)
