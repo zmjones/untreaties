934263f533bccac25673674f4f9e3a4db38fb3e2
Zachary M. Jones <zmj@zmjones.com>
2013-11-18 15:03:30 -0500

    pep8 compliance

--- un_data.py
+++ un_data.py
@@ -8,6 +8,7 @@ import time
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 def read_page(url):
     """
     downloads the page, sleeps for 60 seconds if this fails
@@ -26,9 +27,10 @@ def read_table(url, table_tag):
     then parses the specified html table
     """
     soup = read_page(url)
-    return soup.find(lambda tag:tag.name == "table" and 
-                            tag.has_attr("id") and 
-                            tag["id"] == table_tag)
+    return soup.find(lambda tag: tag.name == "table" and
+                     tag.has_attr("id") and
+                     tag["id"] == table_tag)
+
 
 def get_chap_list(table_tag, base_url):
     """
@@ -36,10 +38,10 @@ def get_chap_list(table_tag, base_url):
     """
     table = read_table(base_url + "ParticipationStatus.aspx", table_tag)
     return [[td.get_text(strip=True)
-             if td.find("a") is None 
-             else base_url + td.find("a")["href"] 
-             for td in tr.find_all("td")] 
-             for tr in table.find_all("tr")]
+            if td.find("a") is None
+            else base_url + td.find("a")["href"]
+            for td in tr.find_all("td")]
+            for tr in table.find_all("tr")]
 
 
 def get_treaty_list(table_tag, base_url, chap_list):
@@ -54,10 +56,12 @@ def get_treaty_list(table_tag, base_url, chap_list):
             number = re.sub("\\.$", "", col[0].get_text(strip=True))
             name = clean(re.sub(r"\xc2|\xa0", "", col[1].get_text(strip=True)))
             url = base_url + col[1].find("a").get("href")
-            df.append([chap_list[chap][0], number, name, url, 
+            df.append([chap_list[chap][0], number, name, url,
                        chap_list[chap][1], chap_list[chap][2]])
-    cols = ["chapter_no", "treaty_no", "treaty_name", "url", "chapter_url", "chapter_name"]
-    pd.DataFrame(df, columns = cols).to_csv("index.csv", index=False, encoding="utf-8")
+    cols = ["chapter_no", "treaty_no", "treaty_name", "url",
+            "chapter_url", "chapter_name"]
+    pd.DataFrame(df, columns=cols).to_csv("index.csv",
+                                          index=False, encoding="utf-8")
     return df
 
 
@@ -77,7 +81,7 @@ def get_xml(treaty_url):
     finds the XML link on a treaty page
     """
     soup = read_page(treaty_url)
-    xml_link = soup.find(lambda tag:tag.name == "a" and
+    xml_link = soup.find(lambda tag: tag.name == "a" and
                          tag.has_attr("id") and
                          tag["id"] == "ctl00_ContentPlaceHolder1_lnkXml")
     return read_page("http://treaties.un.org" + str(xml_link["href"]))
@@ -87,31 +91,31 @@ def get_normal_table(soup):
     """
     fetches data from the typical UN treaty page
     """
-    table = soup.find(lambda tag:tag.name == "participants")
+    table = soup.find(lambda tag: tag.name == "participants")
     if table is None:
         return None
     return [[clean(td.get_text(strip=True))
-             if td.find("a") is None 
-             else base_url + td.find("a")["href"] 
-             for td in tr.find_all("entry")] 
-             for tr in table.find_all("row")]
+            if td.find("a") is None
+            else base_url + td.find("a")["href"]
+            for td in tr.find_all("entry")]
+            for tr in table.find_all("row")]
 
 
 def get_special_table(soup):
     """
     function that fetches special cases that the normal function fails at
     """
-    table = soup.find(lambda tag:tag.name == "specialtables")
+    table = soup.find(lambda tag: tag.name == "specialtables")
     if table is None:
         return None
     header = [[clean(td.get_text(strip=True), True)
-               for td in tr.find_all("title")] 
-               for tr in table.find_all("tableheader")]
+              for td in tr.find_all("title")]
+              for tr in table.find_all("tableheader")]
     data = [[clean(td.get_text(strip=True))
-             if td.find("a") is None 
-             else base_url + td.find("a")["href"] 
-             for td in tr.find_all("column")] 
-             for tr in table.find_all("row")]
+            if td.find("a") is None
+            else base_url + td.find("a")["href"]
+            for td in tr.find_all("column")]
+            for tr in table.find_all("row")]
     return header + data
 
 
@@ -119,13 +123,13 @@ def get_declarations(soup, treaty_id):
     """
     fetches any reservations that countries register for a particular treaty
     """
-    table = soup.find(lambda tag:tag.name == "declarations")
+    table = soup.find(lambda tag: tag.name == "declarations")
     if table is not None:
         for row in table.find_all("declaration"):
             name = clean(row.find("participant").get_text(strip=True), True)
-            text = row.find_all(lambda tag:tag.name == "text" and 
-                                       tag.has_attr("type") and 
-                                       tag["type"] == "para")
+            text = row.find_all(lambda tag: tag.name == "text" and
+                                tag.has_attr("type") and
+                                tag["type"] == "para")
             with open("declarations/" + treaty_id + ".txt", "a+") as f:
                 f.writelines("#" + name.encode("utf-8") + "\n")
                 for entry in text:
@@ -154,10 +158,12 @@ def get_treaties(base_url, treaty_list):
         if not os.path.exists("treaties"):
             os.makedirs("treaties")
         df = pd.DataFrame(df)
-        if not df.empty: 
+        if not df.empty:
             df.ix[:, 0] = df.ix[:, 0].map(lambda x: re.sub("\d|,", "", x))
-            df.to_csv("treaties/" + treaty_id + ".csv", header = False, index = False, encoding = "utf-8")
-        sys.stdout.write(str(treaty) + " of " + str(len(treaty_list)) + " complete\r")
+            df.to_csv("treaties/" + treaty_id + ".csv", header=False,
+                      index=False, encoding="utf-8")
+        sys.stdout.write(str(treaty) + " of " +
+                         str(len(treaty_list)) + " complete\r")
         sys.stdout.flush()
 
 
